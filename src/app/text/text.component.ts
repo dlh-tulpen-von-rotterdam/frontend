304c66472ffdeb6f9c9a684911e2b03dc98287e8
Marco Ghilardelli <marco.ghilardelli@sbb.ch>
2019-06-07 16:43:56 +0200

    - always show text boxes - re translate when changing output language

--- src/app/text/text.component.ts
+++ src/app/text/text.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
-import {SpeechService, TextAnalysis, TranslationResponse} from './speech.service';
-import {BehaviorSubject, Observable, Subscription} from 'rxjs';
+import {SpeechService, TranslationResponse} from './speech.service';
+import {BehaviorSubject, Subscription} from 'rxjs';
 import {debounceTime, distinctUntilChanged, filter, switchMap, tap} from 'rxjs/operators';
 import {TranslateService} from '@ngx-translate/core';
 
@@ -12,7 +12,6 @@ import {TranslateService} from '@ngx-translate/core';
 export class TextComponent {
   public recognisedText$ = new BehaviorSubject<string>('');
   public translatedResult$ = new BehaviorSubject<TranslationResponse>({text: ''});
-  public analyzedText$ = new Observable<TextAnalysis>();
   public listening = false;
   public inputLang = this.translate.currentLang;
   public outputLang = 'de-ch';
@@ -21,52 +20,32 @@ export class TextComponent {
 
   constructor(private speechService: SpeechService,
               private translate: TranslateService) {
-    this.translate.onLangChange.subscribe(value => {
-      this.inputLang = value.lang;
-      if (this.inputLang === this.outputLang) {
-        if (this.outputLang !== 'en') {
-          this.outputLang = 'en';
-        } else {
-          this.outputLang = 'de-ch';
-        }
-      }
-    });
-    this.recognisedText$
-      .pipe(
-        distinctUntilChanged(),
-        debounceTime(200),
-        filter(rt => rt && rt.length > 0),
-        tap(rt => {
-          console.debug('# voice recognition: ', rt);
-        }),
-        switchMap(rt => this.speechService.sendToBackend$(rt, this.inputLang, this.outputLang)),
-        tap(translated => {
-          console.debug('# translation from backend: ', translated);
-          this.translatedResult$.next(translated);
-        }),
-      ).subscribe(() => {
-    });
 
-    this.analyzedText$ = this.recognisedText$
-      .pipe(
-        distinctUntilChanged(),
-        debounceTime(200),
-        filter(rt => rt && rt.length > 0),
-        tap(rt => {
-          console.debug('# voice recognition: ', rt);
-        }),
-        switchMap(rt => this.speechService.analyzeText$(rt, this.inputLang)),
-      );
+    this.translateText();
   }
 
-  private changeLanguage(lan: string): void {
-    this.translate.use(lan);
+  private changeInputLanguage(lang: string): void {
+    this.reset();
+    this.inputLang = lang;
+    if (this.inputLang === this.outputLang) {
+      if (this.outputLang !== 'en') {
+        this.outputLang = 'en';
+      } else {
+        this.outputLang = 'de-ch';
+      }
+    }
+
+    this.translate.use(lang);
+  }
+
+  changeOutputLanguage(lang: string) {
+    this.outputLang = lang;
+    this.translateText();
   }
 
   reset() {
     this.recognisedText$.next('');
     this.translatedResult$.next({text: ''});
-    this.stopListening();
   }
 
   listen() {
@@ -121,5 +100,23 @@ export class TextComponent {
       case 'hu':
         return 'hu-HU';
     }
+  }
+
+  private translateText() {
+    this.recognisedText$
+      .pipe(
+        distinctUntilChanged(),
+        debounceTime(200),
+        filter(rt => rt && rt.length > 0),
+        tap(rt => {
+          console.debug('# voice recognition: ', rt);
+        }),
+        switchMap(rt => this.speechService.sendToBackend$(rt, this.inputLang, this.outputLang)),
+        tap(translated => {
+          console.debug('# translation from backend: ', translated);
+          this.translatedResult$.next(translated);
+        }),
+      ).subscribe(() => {
+    });
   }
 }
