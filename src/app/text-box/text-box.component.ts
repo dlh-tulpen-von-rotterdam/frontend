2a6cdc323cced0e8fb0013081f62dd23c85f0e5b
Marco Ghilardelli <marco.ghilardelli@sbb.ch>
2019-06-12 11:31:24 +0200

    - markup diffs from glossary

--- src/app/text-box/text-box.component.ts
+++ src/app/text-box/text-box.component.ts
@@ -1,12 +1,13 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges, SimpleChanges} from '@angular/core';
 import {SpeechService} from '../text/speech.service';
+import {diff_match_patch} from 'diff-match-patch';
 
 @Component({
   selector: 'app-text-box',
   templateUrl: './text-box.component.html',
   styleUrls: ['./text-box.component.scss']
 })
-export class TextBoxComponent {
+export class TextBoxComponent implements OnChanges {
 
   @Input()
   private title: string;
@@ -23,7 +24,28 @@ export class TextBoxComponent {
   @Input()
   private diff: string;
 
+  private innerHtml;
+
   constructor(private speechService: SpeechService) {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (this.diff) {
+      this.innerHtml = '';
+
+      const Diff = new diff_match_patch();
+      const diffArr = Diff.diff_main(this.diff, this.content);
+      Diff.diff_cleanupSemantic(diffArr);
+
+      diffArr.forEach(value => {
+        if (value[0] === 1) {
+          this.innerHtml += `<span class="emphasize">${value[1]}</span>`;
+        }
+        if (value[0] === 0) {
+          this.innerHtml += value[1];
+        }
+      });
+    }
   }
 
   speak() {
